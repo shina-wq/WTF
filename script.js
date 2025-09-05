4152a586e87ce55111b7308057e4654e25279086
Shina Atete Mpeta <atetempeta@gmail.com>
2025-09-05 09:25:22 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// The variables
+const num1 = 42
+const num2 = 24
+const name = "Shina"
+
+// Addition
+const result = num1 + num2
+console.log(result)
+
+// Subtraction
+console.log(num1 - num2)
+
+// Multiplication
+console.log(num1 * num2)
+
+// Division
+console.log(num1 / num2)
+
+// Result message
+console.log("Hello " + name + ", the result of num1 + num2 is " + result)
